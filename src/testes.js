6041f03cb313e969c2f7dfedba7d8528449bf437
brunau1 <brunosantosprotec@gmail.com>
2019-08-07 16:01:23 -0300

    test to pass an array to object

--- src/testes.js
+++ src/testes.js
@@ -1,43 +1,9 @@
-var catchError = (data) => {
-    var inputUserName = document.getElementById('inputUserName')
-    var errorUserName = document.getElementById('user-name-invalid-feedback')
-    var inputEmail = document.getElementById('inputEmailFirst')
-    var errorEmail = document.getElementById('email-invalid-feedback')
-    var errors = 0
+var array = [['nome', 'Bruno'],['nome', 'Fael']]
+var obj = {}
 
-    if (data) {
-        try {
-            if (data.message.username || data.message.includes('username')) {
-                // alert("Nome de usuário inválido!")
-                message.innerHTML = 'Nome de usuário inválido!'
-                message.hidden = false
+array.forEach(item=>{
+    const [ key, value] = item
+    obj[key] = value
+})
 
-                errorUserName.style.display = 'block'
-                inputUserName.style.borderColor = '#dc3545'
-                inputUserName.value = ''
-                inputUserName.disabled = false
-
-                setTimeout(() => {
-                    message.hidden = true
-                }, 2500)
-            }
-        } catch (e) {
-            console.log(e)
-        }
-        if (data.message.email || data.message.includes('email')) {
-            // alert("O email informado já existe!")
-            message.innerHTML = 'O email informado já existe!'
-            message.hidden = false
-
-            errorEmail.style.display = 'block'
-            inputEmail.style.borderColor = '#dc3545'
-            inputEmail.value = ''
-            inputEmail.disabled = false
-
-            setTimeout(() => {
-                message.hidden = true
-            }, 2500)
-        }
-        backToFirst()
-    }
-}
+//transforma um array de arrays em um objeto
