5738541ebb07449ad65803bb943c0b6a5f3b49bb
brunau1 <brunosantosprotec@gmail.com>
2019-08-08 16:11:28 -0300

    added async email e username verification

--- src/js/formValidator.js
+++ src/js/formValidator.js
@@ -283,6 +283,50 @@ inputPhoneVerification.addEventListener('change', event => {
     }
 })
 
+var validateAsyncEmail = async() => {
+    var asyncEmailVerification = () => {
+        const email = document.querySelector('#inputEmailFirst').value.toString()
+        return new Promise((resolve, reject) => {
+            $.ajax({
+                url: `https://abrasel.dj.emp.br/api/users/exists/email/${email}/`,
+                type: 'get',
+            })
+                .done(data => {
+                    const exists = data.exists == false ? false : true
+                    resolve(exists)
+                })
+                .fail(err => {
+                    reject(err)
+                });
+        })
+    }
+    const exists = await asyncEmailVerification()
+    console.log(`Response result email: ${exists}`)
+    return exists
+}
+
+var validateAsyncUsername = async() => {
+    var asyncUsernameVerification = () => {
+        const username = document.querySelector('#inputUserName').value.toString()
+        return new Promise((resolve, reject) => {
+            $.ajax({
+                url: `https://abrasel.dj.emp.br/api/users/exists/username/${username}/`,
+                type: 'get',
+            })
+                .done(data => {
+                    const exists = data.exists == false ? false : true
+                    resolve(exists)
+                })
+                .fail(err => {
+                    reject(err)
+                });
+        })
+    }    
+    const exists = await asyncUsernameVerification()
+    console.log(`Response result username: ${exists}`)
+    return exists
+}
+
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 window.addEventListener('load', function () {
     selectFieldAndAddPropertie()
@@ -427,8 +471,16 @@ window.addEventListener('load', function () {
                 errorUserName.style.display = 'block'
                 inputUserName.style.borderColor = '#dc3545'
                 document.querySelector('#first-step-button-next').disabled = true
-            } else if (!inputUserName.value.toString()) {
+                document.querySelector('user-name-invalid-feedback').innerHTML = 'O nome deve ter letras minúsculas e sem espaçamento!'
+            } 
+            if (!inputUserName.value.toString()) {
                 document.querySelector('#first-step-button-next').disabled = true
+            } 
+            if(validateAsyncUsername()){
+                errorUserName.style.display = 'block'
+                inputUserName.style.borderColor = '#dc3545'
+                document.querySelector('#first-step-button-next').disabled = true
+                document.querySelector('user-name-invalid-feedback').innerHTML = 'Este nome de usuário já existe!'
             }
             else {
                 errorUserName.style.display = 'none'
@@ -450,10 +502,17 @@ window.addEventListener('load', function () {
                 document.querySelector('#first-step-button-next').disabled = true
                 errorEmail.style.display = 'block'
                 inputEmail.style.borderColor = '#dc3545'
-            } else if (!inputEmail.value.toString()) {
+                document.querySelector('email-invalid-feedback').innerHTML = 'Insira um endereço de email válido!'
+            } 
+            if (!inputEmail.value.toString()) {
                 document.querySelector('#first-step-button-next').disabled = true
-            }
-            else {
+            } 
+            if(validateAsyncEmail()){
+                errorUserName.style.display = 'block'
+                inputUserName.style.borderColor = '#dc3545'
+                document.querySelector('#first-step-button-next').disabled = true
+                document.querySelector('email-invalid-feedback').innerHTML = 'Este endereço email já existe!'
+            } else {
                 errorEmail.style.display = 'none'
                 inputEmail.style.borderColor = '#28a745'
                 if (!checkBox.checked)
--- src/testes.js
+++ src/testes.js
@@ -1,9 +1,54 @@
-var array = [['nome', 'Bruno'],['nome', 'Fael']]
-var obj = {}
+var validateEmail = async() => {
+    var asyncEmailVerification = () => {
+        const email = 'teste@gmail.com'
+        return new Promise((resolve, reject) => {
+            $.ajax({
+                url: `https://abrasel.dj.emp.br/api/users/exists/email/${email}/`,
+                type: 'get',
+            })
+                .done(data => {
+                    const exists = data.exists == false ? false : true
+                    resolve(exists)
+                })
+                .fail(err => {
+                    reject(err)
+                });
+        })
+    }
+    const exists = await asyncEmailVerification()
+    console.log(`Response result email: ${exists}`)
+}
 
-array.forEach(item=>{
-    const [ key, value] = item
-    obj[key] = value
-})
+var validateUsername = async() => {
+    var asyncUsernameVerification = () => {
+        const username = 'teste'
+        return new Promise((resolve, reject) => {
+            $.ajax({
+                url: `https://abrasel.dj.emp.br/api/users/exists/username/${username}/`,
+                type: 'get',
+            })
+                .done(data => {
+                    const exists = data.exists == false ? false : true
+                    resolve(exists)
+                })
+                .fail(err => {
+                    reject(err)
+                });
+        })
+    }    
+    const exists = await asyncUsernameVerification()
+    console.log(`Response result username: ${exists}`)
+}
 
-//transforma um array de arrays em um objeto
+
+//____________transforma um array de arrays em um objeto_________________
+
+// var array = [['nome', 'Bruno'],['nome', 'Fael']]
+// var obj = {}
+
+// array.forEach(item=>{
+//     const [ key, value] = item
+//     obj[key] = value
+// })
+
+//____________transforma um array de arrays em um objeto_________________
