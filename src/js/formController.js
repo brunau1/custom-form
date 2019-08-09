5211ee5de03eb6f4a4cadbf41a3b227cd5b2bfaf
brunau1 <brunosantosprotec@gmail.com>
2019-08-09 09:24:05 -0300

    fixed the cep empty error

--- src/js/formController.js
+++ src/js/formController.js
@@ -43,34 +43,8 @@ var fillCnpjFields = () => {
                 field.value = resultCnpj[data].toLowerCase()
             else
                 field.value = resultCnpj[data]
-
-            // if (!field.value)
-            //     field.disabled = false
-            // else
-            //     field.disabled = true
         }
     })
-
-    // if (resultCnpj['cep']) {
-    //     fieldCep = document.querySelector('#inputCep')
-    //     fieldNumber = document.querySelector('#inputNumber')
-    //     resultCnpj['cep'] = resultCnpj['cep'].replace('.', '').replace('-', '')
-
-    //     fieldCep.value = resultCnpj['cep']
-
-    //     document.getElementById("buttonCep").click()
-    //     fieldNumber.value = resultCnpj['number']
-
-    // if (!fieldCep.value)
-    //     fieldCep.disabled = false
-    // else
-    //     fieldCep.disabled = true
-
-    // if (!fieldNumber.value)
-    //     fieldNumber.disabled = false
-    // else
-    //     fieldNumber.disabled = true
-    //}
 }
 
 var emptyCnpjFields = () => {
@@ -79,21 +53,7 @@ var emptyCnpjFields = () => {
 
         field = document.querySelector(item)
         field.value = ''
-
-        // if (!field.value)
-        //     field.disabled = false
-        // else
-        //     field.disabled = true
     })
-    fieldCep = document.querySelector('#inputCep')
-    fieldNumber = document.querySelector('#inputNumber')
-
-    fieldCep.value = ''
-    fieldNumber.value = ''
-
-    // fieldCep.disabled = false
-    // fieldNumber.disabled = false
-    //emptyAddressFields()
 }
 
 
@@ -104,11 +64,6 @@ var fillAddressFields = () => {
             field = document.querySelector(item)
             let data = item.replace('#input', '').toLowerCase()
             field.value = resultCep[data]
-
-            // if (!field.value)
-            //     field.disabled = false
-            // else
-            //     field.disabled = true
         }
     })
 }
@@ -119,65 +74,9 @@ var emptyAddressFields = () => {
         if (item != '#inputCep' && item != '#inputCountry') {
             field = document.querySelector(item)
             field.value = ''
-
-            // if (!field.value)
-            //     field.disabled = false
-            // else
-            //     field.disabled = true
         }
     })
 }
-
-// var validateFormData = () => {
-//     const select = document.querySelector('#inputBusiness')
-//     error = false
-
-//     if (select.value == 1)
-//     fieldIdsJuridic.map(item => {
-//         error = fieldValidator(item)
-//         if (!error)
-//             return error
-//     })
-
-//     fieldIdsPerson.map(item => {
-//         error = fieldValidator(item)
-//         if (!error)
-//             return error
-//     })
-
-//     fieldIdsAddress.map(item => {
-//         error = fieldValidator(item)
-//         if (!error)
-//             return error
-//     })
-
-//     return error
-// }
-
-// var fieldValidator = (fieldId) => {
-//     // const select = document.querySelector('#inputBusiness')
-//     const field = document.querySelector(fieldId).value
-
-//     if (!field.disabled) {
-//         if (fieldId == '#inputPassword')
-//             if (field.toString().length < 6)
-//                 return true
-
-//         if (fieldId == '#inputSex')
-//             if (field == '')
-//                 return true
-
-//         // if (fieldId == '#inputOffice') {
-//         //     if (select.value == '2')
-//         //         if (field.trim() == "" || field == null)
-//         //             return true;
-//         // }
-
-//         if (field.trim() == "" || field == null)
-//             return true
-//     }
-//     return false
-// }
 
 var showSecondForm = () => {
     if (formCounter == 0) {
@@ -225,11 +124,8 @@ var backToSecond = () => {
 }
 
 window.addEventListener('load', function () {
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
     var nameFirst = document.getElementById('inputNameFirst')
     var nameSecond = document.getElementById('inputNameSecond')
-    // var emailFirst = document.getElementById('inputEmailFirst')
-    // var emailSecond = document.getElementById('inputEmailSecond')
 
     if (nameFirst)
         nameFirst.addEventListener('change', event => {
@@ -244,20 +140,4 @@ window.addEventListener('load', function () {
                 nameFirst.value = nameSecond.value
             }
         }, false)
-
-    // if (emailFirst)
-    //     emailFirst.addEventListener('change', event => {
-    //         if (emailFirst.value.toString().length > 0) {
-    //             emailSecond.value = emailFirst.value
-    //         } else if (emailSecond.value.toString().length > 0) {
-    //             emailFirst.value = emailSecond.value
-    //         }
-    //     }, false)
-
-    // if (emailSecond)
-    //     emailSecond.addEventListener('change', event => {
-    //         if (emailSecond.value.toString().length > 0) {
-    //             emailFirst.value = emailSecond.value
-    //         }
-    //     }, false)
 }, false)
